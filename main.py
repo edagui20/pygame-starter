60ed3f5a53612db9ef962128a915b172ec9f2ae7
Efren Aguilar-Hernandez <edagui20>
2020-02-12 11:17:16 -0500

    I added three images and one text

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ pygame.init()
 # Create the window
 win = pygame.display.set_mode((800, 600))
 
+imge = pygame.image.load('assets\mountains.gif')
+imge = pygame.transform.scale(imge, (800, 450))
+
+
+spritesheet = pygame.image.load('assets\mountains.gif').convert()
+
+img = pygame.image.load('assets\hero\sliced\spellun-sprite.png')
+
+spritesheet = pygame.image.load('assets\hero\sliced\spellun-sprite.png').convert()
+
+prin = pygame.image.load('assets\hero\princess.png').convert_alpha()
+prin = pygame.transform.scale(prin, (50, 80))
+
+spritesheet = pygame.image.load('assets\hero\princess.png').convert_alpha()
+
+font = pygame.font.SysFont("arial", 72)
+text = font.render("The brave knight", True, (255, 255, 255))
+
+
+smol_img = pygame.Surface([16, 16]).convert()
+smol_img.blit(spritesheet, (0, 0), (0, 0, 16, 16))
+
 run = True
 while run:
   for event in pygame.event.get():
@@ -11,10 +33,18 @@ while run:
       run = False
 
 # Game code starts here ---------------------
-  win.fill((0, 0, 0))
+  win.fill((192, 167, 135))
+  win.blit(imge, (0, 0))
+  win.blit(img, (200, 400))
+  win.blit(prin, (500, 410))
+  win.blit(text, (120, 160))
+  
+  
+  
+  
 
-  # Draw a rectangle
-  pygame.draw.rect(win, (0, 204, 102), (50, 50, 100, 200))
+
+
   
   #Update the display
   pygame.display.update()
