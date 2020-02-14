8ecdf94aad83459edc4dde5e441710d6a83a32db
Efren Aguilar-Hernandez <edagui20>
2020-02-14 11:16:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,21 @@ y=400
 spritesheet = pygame.image.load('assets\hero\sliced\spellun-sprite.png').convert()
 
 prin = pygame.image.load('assets\hero\princess.png').convert_alpha()
+w=500
+z=410
+
 prin = pygame.transform.scale(prin, (50, 80))
 
 spritesheet = pygame.image.load('assets\hero\princess.png').convert_alpha()
 
 font = pygame.font.SysFont("arial", 72)
-text = font.render("The brave knight", True, (255, 255, 255))
+text = font.render("Capture the princess", True, (255, 255, 255))
+
+font = pygame.font.SysFont("arial", 20)
+word = font.render("The princess", True, (255, 255, 255))
+
+font = pygame.font.SysFont("arial", 30)
+key = font.render("The knight", True, (255, 255, 255))
 
 
 smol_img = pygame.Surface([16, 16]).convert()
@@ -37,14 +46,18 @@ while run:
       
   keys = pygame.key.get_pressed()
   if keys[pygame.K_LEFT]:
-    x -= 1
+    x -= 0.5
   if keys[pygame.K_RIGHT]:
-    x += 1
+    x += 0.5
   if keys[pygame.K_UP]:
     y -= 1
+    
   if y<410:
     y += 0.5
-  
+    
+  if x == w and y==z:
+    w=x
+    z=y
   
 
 
@@ -52,8 +65,10 @@ while run:
   win.fill((192, 167, 135))
   win.blit(imge, (0, 0))
   win.blit(img, (x, y))
-  win.blit(prin, (500, 410))
-  win.blit(text, (120, 160))
+  win.blit(prin, (w, z))
+  win.blit(text, (80, 160))
+  win.blit(word, (473, 485))
+  win.blit(key, (173, 485))
   keys=pygame.key.get_pressed()
 
   
